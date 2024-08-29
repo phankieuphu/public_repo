f93fccd0dc416e1bda10531866cb8773d44a74c3
[Vietlink] Phu Phan <phankieuphu@gmail.com>
2024-08-29 18:01:30 +0700

    done test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-public
+
+go 1.22.4
+
+require github.com/phankieuphu/go_private v1.2.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/phankieuphu/go_private/config"
+
+func main() {
+	config.Config()
+}
